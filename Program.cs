cfd9a2de4776252eeace684ebe5cc698ae4ad444
Josephonate <131502920+Josephonate@users.noreply.github.com>
2024-08-19 19:30:37 -0400

    starting to add dotnet framework and beginning objects

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>() { "adam", "amy" };
+
+            employees.Add("barbara");
+            employees.Add("billy");
+
+            Console.WriteLine("My employees are:");
+            Console.WriteLine("***************");
+            
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+    }
+}
+
