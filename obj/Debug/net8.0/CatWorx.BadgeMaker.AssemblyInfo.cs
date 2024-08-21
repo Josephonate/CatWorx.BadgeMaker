9993f118388bd9783beddf9753cb5ed2dd43ac0c
Josephonate <131502920+Josephonate@users.noreply.github.com>
2024-08-20 20:17:40 -0400

    created employee program and formated main program to store and display employee info

--- obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4573f4d7ca55302b1156d9a20cabf0471138eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13d47dd572c82f83ab7a57cfe27d1f4774ed11e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
